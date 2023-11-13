ca70bd10b9973f7c724acb3d6d39a41e703db0a3
sopronyuk <soproniuk@protei-lab.ru>
2023-11-13 19:33:09 +0300

    added dependencies 4 logging, db, ktor, exposed

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,11 @@
+val ktorVersion = "2.3.6"
+val exposedVersion = "0.44.1"
+val hikariVersion = "5.0.1"
+
 plugins {
     kotlin("jvm") version "1.9.0"
+    kotlin("plugin.serialization") version "1.9.0"
+    id("io.ktor.plugin") version "2.3.6"
     application
 }
 
@@ -12,6 +18,32 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+    // Ktor
+    implementation("io.ktor:ktor-server-core:$ktorVersion")
+    implementation("io.ktor:ktor-server-netty:$ktorVersion")
+    implementation("io.ktor:ktor-server-status-pages:$ktorVersion")
+    implementation("io.ktor:ktor-server-default-headers:$ktorVersion")
+    // DB -------------------------------------------------
+    // Hikari
+    implementation("com.zaxxer:HikariCP:$hikariVersion")
+    // Postgres
+    implementation("org.postgresql:postgresql:42.2.27")
+    // Exposed
+    implementation("org.jetbrains.exposed:exposed-core:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-crypt:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-dao:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-jdbc:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-kotlin-datetime:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-json:$exposedVersion")
+    // Logging --------------------------------------------
+    implementation("ch.qos.logback:logback-classic:1.2.9")
+}
+
+ktor{
+    docker{
+        localImageName.set("chat-kt")
+        imageTag.set("latest")
+    }
 }
 
 tasks.test {
