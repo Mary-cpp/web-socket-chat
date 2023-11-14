4227ee7208e3d4b3b9c48898323c6bf5502b6502
sopronyuk <soproniuk@protei-lab.ru>
2023-11-14 19:12:00 +0300

    added serialization, ktor content and websockets dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,12 +17,20 @@ repositories {
 }
 
 dependencies {
+    implementation("io.ktor:ktor-server-core-jvm:2.3.6")
+    implementation("io.ktor:ktor-server-websockets-jvm:2.3.6")
     testImplementation(kotlin("test"))
+    // Serialization
+    implementation("io.ktor:ktor-serialization-kotlinx-json:$ktorVersion")
     // Ktor
     implementation("io.ktor:ktor-server-core:$ktorVersion")
+    implementation("io.ktor:ktor-server-auth:$ktorVersion")
     implementation("io.ktor:ktor-server-netty:$ktorVersion")
+    implementation("io.ktor:ktor-server-content-negotiation:$ktorVersion")
     implementation("io.ktor:ktor-server-status-pages:$ktorVersion")
     implementation("io.ktor:ktor-server-default-headers:$ktorVersion")
+    implementation("io.ktor:ktor-server-websockets:$ktorVersion")
+    implementation("io.ktor:ktor-websockets:$ktorVersion")
     // DB -------------------------------------------------
     // Hikari
     implementation("com.zaxxer:HikariCP:$hikariVersion")
